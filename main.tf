fd0705d374bd855d576a74e691d198d8ef4b1333
insanetyagi <vishalrv5236@gmail.com>
2025-04-06 18:17:42 +0530

    Add main.tf

---
+++ main.tf
@@ -0,0 +1,68 @@
+provider "aws" {
+  region = "us-east-1"
+}
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+  enable_dns_support = true
+  enable_dns_hostnames = true
+}
+resource "aws_subnet" "subnet_public" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "us-east-1a"
+  map_public_ip_on_launch = true
+}
+resource "aws_ecs_cluster" "medusa_cluster" {
+  name = "medusa-cluster"
+}
+resource "aws_iam_role" "ecs_execution_role" {
+  name = "ecs-execution-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action    = "sts:AssumeRole"
+        Principal = {
+          Service = "ecs-tasks.amazonaws.com"
+        }
+        Effect    = "Allow"
+        Sid       = ""
+      },
+    ]
+  })
+}
+
+
+resource "aws_ecs_task_definition" "medusa_task" {
+  family                   = "medusa-task"
+  execution_role_arn       = aws_iam_role.ecs_execution_role.arn
+  network_mode             = "awsvpc"
+  requires_compatibilities = ["FARGATE"]
+  cpu                      = "256"
+  memory                   = "512"
+
+  container_definitions = jsonencode([{
+    name      = "medusa-backend"
+    image     = "your_docker_image"  # Replace with your Medusa Docker image or ECR URI
+    essential = true
+    portMappings = [
+      {
+        containerPort = 9000
+        hostPort      = 9000
+        protocol      = "tcp"
+      }
+    ]
+  }])
+}
+resource "aws_ecs_service" "medusa_service" {
+  name            = "medusa-service"
+  cluster         = aws_ecs_cluster.medusa_cluster.id
+  task_definition = aws_ecs_task_definition.medusa_task.arn
+  desired_count   = 1
+  launch_type     = "FARGATE"
+  network_configuration {
+    subnets          = [aws_subnet.subnet_public.id]
+    assign_public_ip = true
+  }
+}
